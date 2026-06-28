1dba5d64f68d1b81a5da3f13e6fd6dc8a4a89c97
LyThaiMinhKhang-22130113 <22130113@st.hcmuaf.edu.vn>
2026-06-28 14:26:28 +0700

    updated review

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -30,10 +30,10 @@ function App() {
 console.log(DetailProduct);
 export default App;
 */
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainLayout from "./layout/mainLayout";
 import LoginLayout from "./layout/loginLayout";
-
+import MyReviewsPage from "./pages/pageReview/myReview"; // Sửa lại đường dẫn nếu cần
 import Home from "./pages/pageHome/home";
 import Login from "./pages/pageLogin/login";
 import SearchResults from "./pages/pageSearchResults/searchResults";
@@ -44,20 +44,7 @@ import Payment from "./pages/pagePayment/payment";
 import DetailProduct from "./pages/pageDetailsProducts/detailProduct";
 import History from "./pages/pageHistory/history";
 import { ConfirmCOD, ConfirmMoMo, ConfirmVNPAY } from "./pages/pagePayment/confirmPages";
-
-// Chặn trang nếu chưa đăng nhập hoặc tài khoản chưa kích hoạt
-const ProtectedRoute = ({ children }) => {
-    const userStr = localStorage.getItem("user");
-    if (!userStr) {
-        return <Navigate to="/login" replace />;
-    }
-    const user = JSON.parse(userStr);
-    if (user.isVerified === false || user.isVerified === 0) {
-        return <Navigate to="/login" replace />;
-    }
-    return children;
-};
-
+import VnpayReturn from "./pages/pagePayment/VnpayReturn";
 function App() {
     return (
         <BrowserRouter>
@@ -69,14 +56,16 @@ function App() {
                     <Route path="/search" element={<SearchResults />} />
                     <Route path="/address" element={<Address />} />
                     <Route path="/account" element={<Account />} />
-                    <Route path="/cart" element={<ProtectedRoute><Cart /></ProtectedRoute>} />
-                    <Route path="/payment" element={<ProtectedRoute><Payment /></ProtectedRoute>} />
+                    <Route path="/cart" element={<Cart />} />
+                    <Route path="/payment" element={<Payment />} />
                     <Route path="/history" element={<History />} />
+                    <Route path="/account/reviews" element={<MyReviewsPage />} />
                     <Route path="/detailproduct/:id" element={<DetailProduct />} />
                 </Route>
                 <Route path="/payment/confirm-cod" element={<ConfirmCOD />} />
-                <Route path="/payment/confirm-momo" element={<ConfirmMoMo />} />
+                {/*<Route path="/payment/confirm-momo" element={<ConfirmMoMo />} />*/}
                 <Route path="/payment/confirm-vnpay" element={<ConfirmVNPAY />} />
+                <Route path="/payment/vnpay-return" element={<VnpayReturn />} />
                 {/* Layout login */}
                 <Route element={<LoginLayout />}>
                     <Route path="/login" element={<Login />} />
